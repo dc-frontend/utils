3870e0010bfe12e7ff5879091a124c503250e7a5
davidcraig <davidcraig1986@gmail.com>
2021-06-19 17:46:04 +0100

    [reduceSumByKey] - Provide support for string numbers using parseFloat or parseInt

--- src/helpers/array.spec.ts
+++ src/helpers/array.spec.ts
@@ -7,6 +7,20 @@ describe('reduceSumByKey', () => {
     expect(reduceSumByKey(testArray, 'a')).toEqual(6)
   })
 
+  test('it will parseFloats if asFloat is true (default)', () => {
+    const testArray = [ { a: 1.5 }, { a: 1.5 }, { a: 2 } ]
+    const result = reduceSumByKey(testArray, 'a')
+    expect(result).toEqual(5)
+    expect(typeof result).toEqual('number')
+  })
+
+  test('it will parseInts if asFloat is false and objects are not already numbers', () => {
+    const testArray = [ { a: "1.4" }, { a: "1.4" }, { a: 2 } ]
+    const result = reduceSumByKey(testArray, 'a', false)
+    expect(result).toEqual(4)
+    expect(typeof result).toEqual('number')
+  })
+
   test('if any elements are null or undefined it should return the remaining sum', () => {
     const testArray = [ { a: 2 }, { a: 4 }, { a: 4 }, null, undefined ]
     expect(reduceSumByKey(testArray, 'a')).toEqual(10)
--- src/helpers/array.ts
+++ src/helpers/array.ts
@@ -1,10 +1,17 @@
-export function reduceSumByKey (arr: object[], key: string) {
+export function reduceSumByKey (arr: object[], key: string, asFloat: boolean = true) {
   return arr.reduce((accumulator, current) => {
     // typeof null === 'object' "for legacy reasons"
     if (current === null) { return accumulator }
     if (typeof current !== 'object') { return accumulator }
     if (!Object.prototype.hasOwnProperty.call(current, key)) { return accumulator }
 
-    return accumulator + current[key]
+    let value = current[key]
+
+    if (typeof value !== 'number') {
+      const parser = asFloat === true ? parseFloat : parseInt
+      value = parser(value)
+    }
+
+    return accumulator + value
   }, 0)
 }
