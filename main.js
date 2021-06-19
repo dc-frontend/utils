f72b58ff5cbfc28d6a1169101ddd6d90e67c3051
davidcraig <davidcraig1986@gmail.com>
2021-06-19 16:44:21 +0100

    Build main.js.

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-export { hasProp } from './dist/helpers/object'
-export { reduceSumByKey } from './dist/helpers/array'
-export { hasValue } from './dist/helpers/misc'
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "hasProp", {
+  enumerable: true,
+  get: function get() {
+    return _object.hasProp;
+  }
+});
+Object.defineProperty(exports, "reduceSumByKey", {
+  enumerable: true,
+  get: function get() {
+    return _array.reduceSumByKey;
+  }
+});
+Object.defineProperty(exports, "hasValue", {
+  enumerable: true,
+  get: function get() {
+    return _misc.hasValue;
+  }
+});
+
+var _object = require("./dist/helpers/object");
+
+var _array = require("./dist/helpers/array");
+
+var _misc = require("./dist/helpers/misc");
