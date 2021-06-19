c9d33782da5818075fa6784e3ce10ccea5305013
davidcraig <davidcraig1986@gmail.com>
2021-06-19 14:22:43 +0100

    Add more config.

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+const NodeEnv = process.env.NODE_ENV
+const BabelIgnore = [
+  '**/__snapshots__/**'
+]
+
+if (NodeEnv === 'test') {
+  // Do nothing
+} else {
+  BabelIgnore.push('**/*.test.js')
+  BabelIgnore.push('**/*.spec.js')
+  BabelIgnore.push('**/*.spec.ts')
+}
+
+module.exports = {
+  presets: [
+    '@babel/preset-typescript'
+  ],
+  ignore: BabelIgnore
+  // plugins: ['react-docgen']
+}
